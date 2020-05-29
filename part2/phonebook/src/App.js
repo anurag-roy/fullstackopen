bb2a50df69e3766c187d9766faed41a21475465e
Anurag Roy <anurag@2pirad.com>
2020-05-29 20:12:22 +0530

    Completed 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,18 @@ import Person from "./components/Person";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const personsToShow = filter
+    ? persons.filter((p) => p.name.toLowerCase().includes(filter.toLowerCase()))
+    : persons;
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -33,9 +41,18 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const changeFilter = (event) => {
+    // console.log(event.target.value);
+    setFilter(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={changeFilter} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={changeNewName} />
@@ -49,7 +66,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((p) => (
+        {personsToShow.map((p) => (
           <Person key={p.name} name={p.name} number={p.number} />
         ))}
       </div>

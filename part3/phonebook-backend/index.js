0c685c82b38ed0318425fb350082c91c7c6568f9
Anurag Roy <anurag@2pirad.com>
2020-06-01 16:12:57 +0530

    Completed 3.2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,33 +2,41 @@ import express from "express";
 
 const app = express();
 
-const persons = {
-  persons: [
-    {
-      name: "Arto Hellas",
-      number: "040-123456",
-      id: 1,
-    },
-    {
-      name: "Ada Lovelace",
-      number: "39-44-5323523",
-      id: 2,
-    },
-    {
-      name: "Dan Abramov",
-      number: "12-43-234345",
-      id: 3,
-    },
-    {
-      name: "Mary Poppendieck",
-      number: "39-23-6423122",
-      id: 4,
-    },
-  ],
-};
+const persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4,
+  },
+];
 
 app.get("/api/persons", (req, res) => {
   res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+  const requestDateTime = new Date().toString();
+  res.send(
+    `<div>
+      Phonebook has info for ${persons.length} people <br /> <br />
+      ${requestDateTime}
+    </div>`
+  );
 });
 
 const PORT = 3001;

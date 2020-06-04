af90507db405cfbc836942734947ee5a99ea94ec
Anurag Roy <anurag@2pirad.com>
2020-06-04 21:55:50 +0530

    Completed 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongp.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://anurag:${password}@anurags-cluster-nqrkb.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  //.then((res) => console.log("Connected succesfully"))
+  .catch((err) => console.log("Couldn't connect. Error: ", err));
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  id: Number,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    console.log("phonebook:")
+    result.forEach((p) => console.log(`${p.name} ${p.number}`));
+    mongoose.connection.close();
+  });
+} else {
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  const person = new Person({
+    name,
+    number,
+    id: Math.floor(Math.random() * 1000),
+  });
+
+  person.save().then((result) => {
+    console.log(`Added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+}

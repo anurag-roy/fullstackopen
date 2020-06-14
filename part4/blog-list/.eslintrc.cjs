c485931dcc79d9372ac49893da8fa7f47a3d749d
Anurag Roy <anurag@2pirad.com>
2020-06-14 19:19:35 +0530

    Completed 4.1

---
+++ part4/blog-list/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    eqeqeq: "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "no-console": 0,
+    "no-unused-vars": ["error", { args: "after-used" }],
+  },
+};
